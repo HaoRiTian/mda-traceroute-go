b7ee82e69fa6c3ecb171b2f21bde0586968689fa
HaoRiTian <HaoRiTian@github.com>
2022-05-10 16:52:49 +0800

    add NewMessage

--- dataStruct/message.go
+++ dataStruct/message.go
@@ -1,7 +1,17 @@
 package dataStruct
 
+import "mda-traceroute-go/util"
+
 type Message struct {
 	MsgType  string `json:"msg-type"`
 	Msg      string `json:"data"`
 	Datetime string `json:"datetime"`
+}
+
+func NewMessage(msgType string, msg string) *Message {
+	return &Message{
+		MsgType:  msgType,
+		Msg:      msg,
+		Datetime: util.FormatNow(),
+	}
 }
